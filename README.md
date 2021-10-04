8e067ee760796c0c9f08bef53b6adb805355cf8e
TrevorAlspach <60957308+TrevorAlspach@users.noreply.github.com>
2021-10-04 18:58:20 -0400

    Added backtrader example

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# COP4521-TradingBot
+# **COP4521-TradingBot**
+
+## **Project Plan** 
+* Backtrader used as main Framework to run trading bot
+* Import Datafeeds into the cerebro instance via pandas dataframes. These will be generated from a database (sqlite/mysql).
+
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-##Initializing the Repository 
+#This is a simple example given by backtrader docs
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import backtrader as bt
+
+if __name__ == '__main__':
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000.0)
+
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.run()
+
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
